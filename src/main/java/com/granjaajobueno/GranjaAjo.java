7f8ee3a5dc3148745fa02aaf6d5125cec4fc2e21
Darken-027 <torut1638@gmail.com>
2025-05-22 20:20:51 -0500

    primer commit

---
+++ src/main/java/com/granjaajobueno/GranjaAjo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.granjaajobueno;
+
+/**
+ *
+ * @author Ahstell
+ */
+public class GranjaAjo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
