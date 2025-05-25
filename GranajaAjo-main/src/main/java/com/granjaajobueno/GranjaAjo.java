5f4546fcbe974b68ded8b2743adaa90c70b58f9c
ashura <ashurasan04@gmail.com>
2025-05-25 04:51:28 -0500

    nuevas actualizaciones en la parte logica del usuario

--- GranajaAjo-main/src/main/java/com/granjaajobueno/GranjaAjo.java
+++ GranajaAjo-main/src/main/java/com/granjaajobueno/GranjaAjo.java
@@ -1,16 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.granjaajobueno;
 
-/**
- *
- * @author Ahstell
- */
 public class GranjaAjo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public GranjaAjo() {
+    }
+    
 }
