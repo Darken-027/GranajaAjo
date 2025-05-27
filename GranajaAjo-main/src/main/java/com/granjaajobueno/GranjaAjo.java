9c8231502f99b29bb8793f12922f46ddbb877393
ashura <ashurasan04@gmail.com>
2025-05-27 00:12:53 -0500

    generando un iniciador desde el archivo main para evitar errores de codigo

--- GranajaAjo-main/src/main/java/com/granjaajobueno/GranjaAjo.java
+++ GranajaAjo-main/src/main/java/com/granjaajobueno/GranjaAjo.java
@@ -1,13 +1,19 @@
 
 package com.granjaajobueno;
 
+import com.granjaajobueno.gui.MainMenu;
+
 public class GranjaAjo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        //Creando variable y metodos para que se abra la aplicacion directamente del archivo main y no desde las interfaces
+        
+        MainMenu newmenu = new MainMenu();
+        newmenu.setVisible(true);
+        newmenu.setLocationRelativeTo(null);
+        
     }
 
-    public GranjaAjo() {
-    }
     
 }
