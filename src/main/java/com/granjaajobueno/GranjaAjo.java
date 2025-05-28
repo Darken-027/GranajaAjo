122796346e5ac4775466a6df10bae900e836e663
Darken-027 <torut1638@gmail.com>
2025-05-27 23:23:50 -0500

    guardando cambias para poder validar el pull de eduardo

--- src/main/java/com/granjaajobueno/GranjaAjo.java
+++ src/main/java/com/granjaajobueno/GranjaAjo.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.granjaajobueno;
 
-/**
- *
- * @author Ahstell
- */
+
+
 public class GranjaAjo {
 
     public static void main(String[] args) {
